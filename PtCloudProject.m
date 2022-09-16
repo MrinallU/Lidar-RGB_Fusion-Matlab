4ccfc1aa55fe01cf3cb963c281ad4a03742dddf0
Mrinall Umasudhan <mrinalluma@gmail.com>
2022-09-15 20:27:48 -0400

    Update PtCloudProject.m

--- PtCloudProject.m
+++ PtCloudProject.m
@@ -2,21 +2,21 @@
 
 pcapFileName = "dataLID.pcap";
 calibFileName = "dataLID.json";
-im = imread("environment_img.jpg"); 
+im = imread("frame.png"); 
 
 ousterReader = ousterFileReader(pcapFileName,calibFileName);
-frameTime = ousterReader.StartTime + seconds(3);
+frameTime = ousterReader.StartTime + seconds(23);
 [ptCloud,pcatt] = readFrame(ousterReader,frameTime);
 p1 = pcdownsample(ptCloud,'gridAverage',0.5);
 
 % CAMERA PARAMS 
 theta = 0;  
-focalLengthX = 1.98359898e+03; 
-focalLengthY = 1.98783529e+03; 
-imageCenterX = 9.34531049e+02; 
-imageCenterY =  5.63150590e+02; 
-imageResX = 4000; 
-imageResY = 3000; 
+focalLengthX = 1983.59898; 
+focalLengthY = 1987.83529;
+imageCenterX = 563; 
+imageCenterY =  934; 
+imageResX = 1080; 
+imageResY = 1920; 
 
 focalLength    = [focalLengthX, focalLengthY]; 
 principalPoint = [imageCenterX, imageCenterY]; % cx, cy
@@ -25,15 +25,17 @@ imageSize      = [imageResX, imageResY]; % row, col
 
 % ASSEMBLING MATRICIES
 intrinsics = cameraIntrinsics(focalLength,principalPoint,imageSize); 
-rot = [ cosd(theta) sind(theta) 0; ...
-       -sind(theta) cosd(theta) 0; ...
-      0 0 1]; 
-trans = [-0.2 11.2 -1];
-camToLidar = rigid3d(rot, trans); 
-lidarToCam = invert(camToLidar);
+disp(intrinsics); 
+rot = [0 -1 0; ...
+       0 0 -1; ...
+      1 0 0]; 
+%trans = [0.00508 -0.28448 0.0254];
+trans = [0.00508 -0.28448 0.0254];
+lidarToCam = rigid3d(rot, trans); 
 
 
 imPts = projectLidarPointsOnImage(p1,intrinsics,lidarToCam);
+%disp(imPts);   
 figure
 imshow(im)
 hold on
